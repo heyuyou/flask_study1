4a15666da2ddc9ac3768f943f8fcfa2a4faa8f69
heyuyou <heyuyou888@163.com>
2022-03-31 15:53:12 +0800

    url_for 测试

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -7,10 +7,18 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 
+
 @app.route("/hello2/<name>")
 @app.route("/hello2")
 def hello2(name='he'):
     return "hello %s" % name
+
+
+@app.route("/test")
+def test():
+    print(url_for("hello2"))
+    return "test"
+
 
 if __name__ == '__main__':
     app.run()
